a1b4c07eed4df779806e5c1eaa4bdfc91d632e9d
MatrosovDenis <51385717+MatrosovDenis@users.noreply.github.com>
2019-06-19 14:27:56 +0300

    Update
    19.06.2019 14:26

--- lav12_1.c
+++ lav12_1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "labs.h"
+#include "lab12.h"
 
 head* getCH(){
     head* h;
--- main.c
+++ main.c
@@ -4,18 +4,21 @@
 #include "lab11.h"
 #include "lab12.h"
 
-int main(){
-    int N;
-    scanf("%d", &N);
-    switch(N){
-case 10:
-     lab10();
+int main()
+{
+  int N;
+  printf("Please enter number of lab: ");
+  scanf("%d",&N);
+  system("cls");
+  switch(N){
+  case 11:
+      lab11();
+      break;
+  case 10:
+      lab10();
     break;
-case 11:
-     lab11();
-    break;
-case 12:
-     lab12();
+  case 12:
+      lab12();
     break;
     }
     return 0;
